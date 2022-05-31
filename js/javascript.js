8dc64392ce93984d829b46bfc9a2c9b5ac555cf9
CetO-hub <skynet@SkyNet.localdomain>
2022-05-31 18:26:06 +0200

    Add javascript for overlay and form pop-up

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,27 @@
+let addBookButton = document.querySelector(".add-book");
 let isOverlay = document.querySelector(".overlay");
+let isBookFormContainer = document.querySelector(".book-form-container");
+
+const addActive = () => {
+  isOverlay.classList.add("active");
+  isBookFormContainer.classList.add("active");
+};
+
+const removeActive = () => {
+  isOverlay.classList.remove("active");
+  isBookFormContainer.classList.remove("active");
+};
+
+addBookButton.addEventListener("click", () => {
+  addActive();
+});
 
 isOverlay.addEventListener("click", () => {
-  isOverlay.classList.add("active");
+  removeActive();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    removeActive();
+  }
 });
